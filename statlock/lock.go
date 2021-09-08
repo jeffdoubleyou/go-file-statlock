38ceb3d250806c8c01aaf61c561bf2d422205a78
Jeffrey Weitz <jeffdoubleyou@gmail.com>
2021-09-08 14:27:15 -0400

    Initial

---
+++ statlock/lock.go
@@ -0,0 +1,143 @@
+package statlock
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+const (
+	StatusLocked   = 1
+	StatusWaiting  = 2
+	StatusUnlocked = 3
+	StatusOrphaned = 4
+	StatusTimeout  = 5
+	StatusFailed   = 6
+	StatusRelease  = 99
+)
+
+type Lock struct {
+	Path            string
+	Duration        int
+	Status          int
+	MaxWaitInterval int
+	file            *os.File
+}
+
+type StatLock interface {
+	Lock() (bool, error)
+	Unlock() (bool, error)
+	WaitForLock() (bool, error)
+	openLock() (bool, error)
+	keepLock()
+	refreshLock() (bool, error)
+}
+
+func NewLock(path string, seconds int, maxWaitInterval ...int) *Lock {
+	waitInterval := 10
+	if len(maxWaitInterval) == 1 {
+		waitInterval = maxWaitInterval[0]
+		fmt.Printf("Setting max wait interval to %d", waitInterval)
+	}
+
+	return &Lock{
+		Path:            path,
+		Duration:        seconds,
+		MaxWaitInterval: waitInterval,
+	}
+}
+
+func (l *Lock) Lock() (bool, error) {
+	if _, err := l.openLock(); err != nil {
+		return false, err
+	}
+	l.Status = StatusLocked
+	go l.keepLock()
+	return true, nil
+}
+
+func (l *Lock) Unlock() (bool, error) {
+	l.Status = StatusRelease
+	unlockAttempts := 0
+	maxAttempts := 10
+	for l.Status == StatusRelease && unlockAttempts < maxAttempts {
+		time.Sleep(time.Duration(500) * time.Millisecond)
+		unlockAttempts++
+	}
+	if l.Status == StatusUnlocked {
+		return true, nil
+	}
+	return false, fmt.Errorf("unable to release lock err %d", l.Status)
+}
+
+func (l *Lock) WaitForLock() (bool, error) {
+	l.Status = StatusWaiting
+	unlockAttempts := 0
+
+	for l.Status != StatusLocked && (unlockAttempts < l.MaxWaitInterval || l.MaxWaitInterval == 0) {
+		if locked, err := l.Lock(); locked {
+			return true, nil
+		} else {
+			fmt.Println(err.Error())
+		}
+		unlockAttempts++
+		time.Sleep(time.Duration(l.Duration) * time.Second)
+	}
+	l.Status = StatusTimeout
+	return false, fmt.Errorf("timeout waiting for lock")
+}
+
+func (l *Lock) openLock() (bool, error) {
+	if fileInfo, err := os.Stat(l.Path); err != nil {
+		if file, err := os.Create(l.Path); err != nil {
+			return false, err
+		} else {
+			l.file = file
+			return true, nil
+		}
+	} else {
+		now := time.Now().Local()
+		if fileInfo.ModTime().Local().Add(time.Duration(l.Duration*2) * time.Second).Before(now) {
+			return l.refreshLock()
+		} else {
+			l.Status = StatusFailed
+			return false, fmt.Errorf("could not take ownership of lock")
+		}
+	}
+}
+
+func (l *Lock) keepLock() {
+	for l.Status != StatusRelease {
+		time.Sleep(time.Duration(l.Duration) * time.Second)
+		if _, err := l.refreshLock(); err != nil {
+			l.Status = StatusRelease
+			fmt.Printf("Unable to update lock file: %s", err.Error())
+		}
+	}
+
+	fmt.Printf("Exited keep lock loop with status: %d\n", l.Status)
+	if _, err := os.Stat(l.Path); err == nil {
+		fmt.Printf("Lock file '%s' exists\n", l.Path)
+		if err := os.Remove(l.Path); err != nil {
+			fmt.Printf("could not remove lock file '%s': %s\n", l.Path, err.Error())
+			l.Status = StatusOrphaned
+		} else {
+			fmt.Printf("Removed lock file succesfully\n")
+			l.Status = StatusUnlocked
+		}
+	} else if os.IsNotExist(err) { // This could occur if another process is waiting for a lock and acquires it while we are unlocking
+		fmt.Printf("lock file '%s' does not exist\n", l.Path)
+		l.Status = StatusUnlocked
+	} else {
+		fmt.Printf("failed to access lock file '%s': %s\n", l.Path, err.Error())
+		l.Status = StatusOrphaned
+	}
+}
+
+func (l *Lock) refreshLock() (bool, error) {
+	t := time.Now().Local()
+	if err := os.Chtimes(l.Path, t, t); err != nil {
+		return false, err
+	}
+	return true, nil
+}
---
+++ statlock/lock_test.go
@@ -0,0 +1,238 @@
+package statlock
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestLock_Lock(t *testing.T) {
+	type fields struct {
+		Path     string
+		Duration int
+		Status   int
+		file     *os.File
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    bool
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := Lock{
+				Path:     tt.fields.Path,
+				Duration: tt.fields.Duration,
+				Status:   tt.fields.Status,
+				file:     tt.fields.file,
+			}
+			got, err := l.Lock()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Lock() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Lock() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLock_Unlock(t *testing.T) {
+	type fields struct {
+		Path     string
+		Duration int
+		Status   int
+		file     *os.File
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    bool
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := Lock{
+				Path:     tt.fields.Path,
+				Duration: tt.fields.Duration,
+				Status:   tt.fields.Status,
+				file:     tt.fields.file,
+			}
+			got, err := l.Unlock()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Unlock() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Unlock() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func sleepLock(t *testing.T, lock *Lock, seconds int, w *sync.WaitGroup) {
+	t.Logf("Sleep for %d seconds", seconds)
+	time.Sleep(time.Duration(seconds) * time.Second)
+	if _, err := lock.Unlock(); err != nil {
+		fmt.Printf("Got error unlocking file: %s\n", err.Error())
+		t.Error(err.Error())
+	} else {
+		t.Logf("Unlocked file: %d", lock.Status)
+	}
+	w.Done()
+}
+
+func TestLock_WaitForLock(t *testing.T) {
+	type args struct {
+		path    string
+		seconds int
+		sleep   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Lock
+	}{
+		{
+			name: "InitialLock",
+			args: args{
+				path:    "testlock.pid",
+				seconds: 5,
+				sleep:   10,
+			},
+			want: &Lock{
+				Path:            "testlock.pid",
+				Duration:        5,
+				Status:          0,
+				MaxWaitInterval: 10,
+			},
+		},
+		{
+			name: "WaitForInitialLock",
+			args: args{
+				path:    "testlock.pid",
+				seconds: 5,
+			},
+			want: &Lock{
+				Path:            "testlock.pid",
+				Duration:        5,
+				Status:          0,
+				MaxWaitInterval: 10,
+			},
+		},
+	}
+	var wg sync.WaitGroup
+	for _, tt := range tests {
+		wg.Add(1)
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewLock(tt.args.path, tt.args.seconds); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewLock() = %v, want %v", got, tt.want)
+			} else {
+				if _, err := got.WaitForLock(); err != nil {
+					t.Errorf("Unable to lock file: %s", err.Error())
+				} else {
+					t.Logf("Lock aquired with status %d", got.Status)
+					if tt.args.sleep > 0 {
+						t.Logf("Run sleep in background")
+						go sleepLock(t, got, tt.args.sleep, &wg)
+					} else {
+						if _, err := got.Unlock(); err != nil {
+							t.Error(err.Error())
+						} else {
+							t.Logf("Unlocked file: %d", got.Status)
+						}
+						wg.Done()
+						wg.Wait()
+					}
+				}
+			}
+		})
+	}
+	wg.Wait()
+}
+func TestLock_openLock(t *testing.T) {
+	type fields struct {
+		Path     string
+		Duration int
+		Status   int
+		file     *os.File
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    bool
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := Lock{
+				Path:     tt.fields.Path,
+				Duration: tt.fields.Duration,
+				Status:   tt.fields.Status,
+				file:     tt.fields.file,
+			}
+			got, err := l.openLock()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("openLock() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("openLock() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewLock(t *testing.T) {
+	type args struct {
+		path    string
+		seconds int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Lock
+	}{
+		{
+			name: "NewLock",
+			args: args{
+				path:    "testlock.pid",
+				seconds: 5,
+			},
+			want: &Lock{
+				Path:     "testlock.pid",
+				Duration: 5,
+				Status:   0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewLock(tt.args.path, tt.args.seconds); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewLock() = %v, want %v", got, tt.want)
+			} else {
+				if _, err := got.Lock(); err != nil {
+					t.Errorf("Unable to lock file: %s", err.Error())
+				} else {
+					t.Logf("Lock aquired with status %d", got.Status)
+					if _, err := got.Unlock(); err != nil {
+						t.Error(err.Error())
+					} else {
+						t.Logf("Unlocked file: %d", got.Status)
+					}
+				}
+			}
+		})
+	}
+}
